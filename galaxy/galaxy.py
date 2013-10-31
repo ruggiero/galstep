87dd36b99d8272c54cd309936755eb64249c9b83
Rafael R <rafael.ruggiero@usp.br>
2013-10-31 17:45:53 -0200

    Positions sampling implemented
    For all the three components - halo, disk and bulge. The script
    works, but the initial conditions it genererates has all the
    velocities set to zero.

---
+++ galaxy/galaxy.py
@@ -0,0 +1,137 @@
+# Use: python galaxy.py [FILE]
+
+from os import path
+from sys import exit
+
+import numpy as np
+import numpy.random as nprand
+from scipy.optimize import brentq
+
+from snapwrite import process_input, write_snapshot
+
+
+halo_core = True
+bulge_core = True
+G = 43007.1
+
+
+def main():
+    init()
+    galaxy_data = generate_galaxy()
+    write_input_file(galaxy_data)
+
+
+def generate_galaxy():
+    coords_halo = set_halo_positions()
+    coords_disk = set_disk_positions()
+    coords_bulge = set_bulge_positions()
+    coords = np.concatenate((coords_halo, coords_disk, coords_bulge))
+    vels = set_velocities(coords)
+    return [coords, vels]
+
+
+def init():
+    global M_halo, M_disk, M_bulge
+    global N_halo, N_disk, N_bulge
+    global a_halo, a_bulge, Rd, z0
+    global N_total, M_total
+
+    if not path.isfile("header.txt") and path.isfile("galaxy_param.txt"):
+        print "header.txt or galaxy_param.txt missing."
+        exit(0)
+
+    vars_ = process_input("galaxy_param.txt")
+    M_halo, M_disk, M_bulge = (float(i[0]) for i in vars_[0:3])
+    N_halo, N_disk, N_bulge = (float(i[0]) for i in vars_[3:6])
+    a_halo, a_bulge, Rd, z0 = (float(i[0]) for i in vars_[6:10])
+    M_total = M_disk + M_bulge + M_halo
+    N_total = N_disk + N_bulge + N_halo
+
+def dehnen_inverse_cumulative(Mc, M, a, core):
+    if(core):
+        return ((a * (Mc**(2/3.)*M**(4/3.) + Mc*M + Mc**(4/3.)*M**(2/3.))) /
+                   (Mc**(1/3.) * M**(2/3.) * (M-Mc)))
+    else:
+        return (a * ((Mc*M)**0.5 + Mc)) / (M-Mc)
+
+
+def set_halo_positions():
+    # The factor M * 200^2 / 201^2 restricts the radius to 200 * a.
+    radii = dehnen_inverse_cumulative(nprand.sample(N_halo) *
+        ((M_halo*40000) / 40401), M_halo, a_halo, halo_core)
+    thetas = np.arccos(nprand.sample(N_halo)*2 - 1)
+    phis = 2 * np.pi * nprand.sample(N_halo)
+    xs = radii * np.sin(thetas) * np.cos(phis)
+    ys = radii * np.sin(thetas) * np.sin(phis)
+    zs = radii * np.cos(thetas)
+
+    # Older NumPy versions freak out without this line.
+    coords = np.column_stack((xs, ys, zs))
+    coords = np.array(coords, order='C')
+    coords.shape = (1, -1) # Linearizing the array.
+    return coords[0]
+
+
+def set_bulge_positions():
+    radii = dehnen_inverse_cumulative(nprand.sample(N_bulge) *
+        ((M_bulge*40000) / 40401), M_bulge, a_bulge, bulge_core)
+    thetas = np.arccos(nprand.sample(N_bulge)*2 - 1)
+    phis = 2 * np.pi * nprand.sample(N_bulge)
+    xs = radii * np.sin(thetas) * np.cos(phis)
+    ys = radii * np.sin(thetas) * np.sin(phis)
+    zs = radii * np.cos(thetas)
+    coords = np.column_stack((xs, ys, zs))
+    coords = np.array(coords, order='C')
+    coords.shape = (1, -1)
+    return coords[0]
+
+
+def set_disk_positions():
+    # TODO: restrict the maximum radius and height
+    radii = np.zeros(N_disk)
+    sample = nprand.sample(N_disk)
+    for i, s in enumerate(sample):
+        radii[i] = disk_radial_inverse_cumulative(s)
+    
+    zs = disk_height_inverse_cumulative(nprand.sample(N_disk))
+    phis = 2 * np.pi * nprand.sample(N_disk)
+
+    xs = radii * np.cos(phis)
+    ys = radii * np.sin(phis)
+
+    coords = np.column_stack((xs, ys, zs))
+    coords = np.array(coords, order='C')
+    coords.shape = (1, -1)
+    return coords[0]
+
+
+def disk_radial_cumulative(r):
+    return (Rd**2-(Rd**2+r*Rd)*np.exp(-r/Rd))/Rd**2
+
+
+# frac is a number between 0 and 1
+def disk_radial_inverse_cumulative(frac):
+    return brentq(lambda r: disk_radial_cumulative(r) - frac, 0, 1.0e10)
+
+
+def disk_height_inverse_cumulative(frac):
+    return 0.5 * z0 * np.log(frac/(1-frac))
+
+
+def set_velocities(coords):
+    potential_tabulated = []
+    return np.zeros(N_total)
+
+
+def write_input_file(galaxy_data):
+    coords = galaxy_data[0]
+    vels = galaxy_data[1]
+    masses = np.empty(N_total)
+    masses.fill(M_total / N_total)
+    ids = np.arange(1, N_total + 1, 1)
+    write_snapshot(n_part=[0, N_halo, N_disk, N_bulge, 0, 0], from_text=False,
+                   data_list=[coords, vels, ids, masses])
+
+
+if __name__ == '__main__':
+    main()
---
+++ galaxy/snapwrite.py
@@ -0,0 +1,144 @@
+'''
+DESCRIPTION:
+
+Script that actually writes a snapshot, either from a set of text files,
+or by directly passing the necessary data to the function write_snapshot
+(for details on the syntax, read the function).
+
+In the first case, just run the following:
+
+python snapwrite.py FOLDER OUTPUT
+
+FOLDER: folder containing all the input files, which are:
+
+    header.txt position.txt velocity.txt id.txt masses.txt (always)
+    energy.txt density.txt smoothing.txt (in case there is gas)
+
+Note that the columns of the positions and velocities files should be
+separated with tabs, as well as the columns of the header. Among the
+input files, header.txt is the only one that accepts comments, which
+should be preceded by a #.
+
+OUTPUT: name of the file that will be the output snapshot.
+'''
+
+import sys
+import os
+import struct
+
+import numpy as np
+
+
+output = sys.argv[-1]
+
+
+# Creates a list containing the non-commented, non-empty lines
+# of the input file for the header.
+def process_input(file_):
+    h_file = []
+    input_ = open(file_, 'r')
+    for line in input_:
+        if line.find("#") != -1:
+            continue
+        elif line.find("\n") == 0:
+            continue
+        else:
+            h_file.append(line.split('\t'))
+    return h_file
+
+
+def read_header(folder, n_part):
+    h_file = process_input(folder + "/header.txt")
+    h_data = []
+    for j in n_part: # n_part
+        h_data.append(int(j))
+    for j in h_file[0][0:6]: # mass
+        h_data.append(float(j))
+    h_data.append(float(h_file[1][0])) # time
+    h_data.append(float(h_file[2][0])) # redshift
+    h_data.append(int(h_file[3][0])) # flag_sfr
+    h_data.append(int(h_file[4][0])) # flag_feedback
+    for j in n_part:
+        h_data.append(int(j)) # n_part_total
+    h_data.append(int(h_file[5][0])) # flag_coooling
+    h_data.append(int(h_file[6][0])) # num_files
+    h_data.append(float(h_file[7][0])) # box_size
+    h_data.append(float(h_file[8][0])) # omega0
+    h_data.append(float(h_file[9][0])) # omega_lambda
+    h_data.append(float(h_file[10][0])) # hubble_param
+
+    # blank, present in the header
+    for i in np.arange(96):
+        h_data.append('\0')
+    s = struct.Struct('iiiiii dddddd d d i i iiiiii i i dddd cccccccccccc\
+                       cccccccccccccccccccccccccccccccccccccccccccccccccc\
+                       cccccccccccccccccccccccccccccccccc')
+    packed_data = s.pack(*h_data)
+    return packed_data
+
+
+def write_dummy(f, values_list):
+    for i in values_list:
+        dummy = [i]
+        s = struct.Struct('i')
+        d = s.pack(*dummy)
+        f.write(d)
+
+
+def write_block(f, block_data, data_type, block_name):
+    write_dummy(f, [8])
+    f.write(struct.pack('c' * 4, *block_name))
+    if(block_name == 'HEAD'):
+        nbytes = 256
+    else:
+        fmt = data_type * len(block_data)
+        nbytes = len(block_data) * 4
+    write_dummy(f, [nbytes + 8, 8, nbytes]) 
+    if(block_name == 'HEAD'):
+        f.write(block_data) 
+    else:
+        f.write(struct.pack(fmt, *block_data))
+    write_dummy(f, [nbytes])
+
+
+def write_snapshot(n_part, folder=None, from_text=True, data_list=None):
+    N_gas = n_part[0]
+    if(from_text and not folder):
+        print ("error: can't call write_snapshot with from_text=True\n"
+               "and without an input files folder.")
+    if(not from_text):
+        folder = os.getcwd()
+
+    # Erasing the input file before opening it.
+    open(output, 'w').close()
+    f = file(output, 'a')
+    header_data = read_header(folder, n_part)
+    if(from_text):
+        pos_data = np.fromfile(folder + "position.txt", sep='\t')
+        vel_data = np.fromfile(folder + "velocity.txt", sep='\t')
+        ID_data = np.fromfile(folder + "id.txt", dtype=int, sep='\t')
+        mass_data = np.fromfile(folder + "masses.txt", sep='\t')
+        if(N_gas > 0):
+            U_data = np.fromfile(folder + "energy.txt", sep='\t')
+            rho_data = np.fromfile(folder + "density.txt", sep='\t')
+            smoothing_data = np.fromfile(folder + "smoothing.txt", sep='\t')
+    else:
+        pos_data = data_list[0]
+        vel_data = data_list[1]
+        ID_data = data_list[2]
+        mass_data = data_list[3]
+        if(N_gas > 0):
+            U_data = data_list[4]
+            rho_data = data_list[5]
+            smoothing_data = data_list[6]
+
+    write_block(f, header_data, None, 'HEAD')
+    write_block(f, pos_data, 'f', 'POS ')
+    write_block(f, vel_data, 'f', 'VEL ')
+    write_block(f, ID_data, 'i', 'ID  ')
+    write_block(f, mass_data, 'f', 'MASS')
+    if(N_gas > 0):
+        write_block(f, U_data, 'f', 'U   ')
+        write_block(f, rho_data, 'f', 'RHO ')
+        write_block(f, smoothing_data, 'f', 'HSML')
+    f.close()
